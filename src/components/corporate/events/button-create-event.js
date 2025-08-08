f70363c7af5f47b566795f84fe40351c47164947
Miguel Payá Vañó <biglodevelop@gmail.com>
2025-08-08 13:48:24 +0200

    [MOD] translated text

--- src/components/corporate/events/button-create-event.js
+++ src/components/corporate/events/button-create-event.js
@@ -26,6 +26,7 @@ import { createEvent } from '../../../services/event';
 import { COLORS, DARK_COLORS } from '../../../colors/colors';
 import MapSearcher from '../../base/map-searcher';
 import { ImageGalleryUpload } from '../../base/image-gallery-upload.js';
+import { useTranslation } from 'react-i18next';
 
 
 
@@ -132,45 +133,47 @@ export const ButtonCreateEvent = ({ refreshEvents }) => {
     };
 
 
+    const { t } = useTranslation("global")
+
 
     return (
         <>
-            <Button onClick={onOpen} bg={DARK_COLORS.gridyellow} color={'black'} _hover={{ bg: 'gray.100' }} fontSize={{ base: 12, md: 16 }} mr={2}>Crear Evento</Button>
+            <Button onClick={onOpen} bg={DARK_COLORS.gridyellow} color={'black'} _hover={{ bg: 'gray.100' }} fontSize={{ base: 12, md: 16 }} mr={2}>{t('profileUser.events.createEvent')}</Button>
             <Modal isOpen={isOpen} onClose={closeModal} size='xl'>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>{step === 1 ? "Paso 1: Información Básica" : "Paso 2: Detalles del Evento"}</ModalHeader>
+                    <ModalHeader>{step === 1 ? t('profileUser.events.1.title') : t('profileUser.events.2.title')}</ModalHeader>
                     <ModalCloseButton onClick={closeModal} />
                     <ModalBody py={5}>
                         {step === 1 && (
                             <>
                                 <Box>
-                                    <Text fontWeight="bold" mb={4}>Galería de Imágenes</Text>
+                                    <Text fontWeight="bold" mb={4}>{t('profileUser.events.1.imageGallery')}</Text>
                                     <ImageGalleryUpload url={`image/upload`} setGalleryImages={setGallery} />
                                 </Box>
                                 <Text fontSize='12'>
-                                    Nombre del evento
+                                    {t('profileUser.events.1.name')}
                                 </Text>
                                 <Input
-                                    placeholder="Nombre del evento"
+                                    placeholder={t('profileUser.events.1.nameEvent')}
                                     value={name}
                                     onChange={(e) => setName(e.target.value)}
                                     mb={4}
                                 />
                                 <Text fontSize='12'>
-                                    Descripción
+                                    {t('profileUser.events.1.description')}
                                 </Text>
                                 <Textarea
-                                    placeholder="Descripción del evento"
+                                    placeholder={t('profileUser.events.1.descriptionEvent')}
                                     value={description}
                                     onChange={(e) => setDescription(e.target.value)}
                                     mb={4}
                                 />
                                 <Text fontSize='12'>
-                                    Link
+                                    {t('profileUser.events.1.link')}
                                 </Text>
                                 <Input
-                                    placeholder="Enlace del evento"
+                                    placeholder={t('profileUser.events.1.linkToEvent')}
                                     value={link}
                                     onChange={(e) => setLink(e.target.value)}
                                     mb={4}
@@ -179,38 +182,38 @@ export const ButtonCreateEvent = ({ refreshEvents }) => {
                                 <Flex w='full' gap={2}>
                                     <Box w={'30%'}>
                                         <Text fontSize='12'>
-                                            Aforo máximo
+                                            {t('profileUser.events.1.maxCapacity')}
                                         </Text>
                                         <NumberInput
                                             value={maximumCapacity}
                                             onChange={(valueString) => setMaximumCapacity(valueString)}
                                             mb={4}
                                         >
-                                            <NumberInputField placeholder="Aforo" />
+                                            <NumberInputField placeholder={t('profileUser.events.1.capacity')} />
                                         </NumberInput>
                                     </Box>
                                     <Box w={'30%'}>
                                         <Text fontSize='12'>
-                                            Duración (H)
+                                            {t('profileUser.events.1.durationH')}
                                         </Text>
                                         <NumberInput
                                             value={duration}
                                             onChange={(valueString) => setDuration(valueString)}
                                             mb={4}
                                         >
-                                            <NumberInputField placeholder="Duración (H)" />
+                                            <NumberInputField placeholder={t('profileUser.events.1.durationH')} />
                                         </NumberInput>
                                     </Box>
                                     <Box w={'30%'}>
                                         <Text fontSize='12'>
-                                            Precio
+                                            {t('profileUser.events.1.price')}
                                         </Text>
                                         <NumberInput
                                             value={price}
                                             onChange={(valueString) => setPrice(valueString)}
                                             mb={4}
                                         >
-                                            <NumberInputField placeholder="Precio" />
+                                            <NumberInputField placeholder={t('profileUser.events.1.price')} />
                                         </NumberInput>
                                     </Box>
                                 </Flex>
@@ -219,7 +222,7 @@ export const ButtonCreateEvent = ({ refreshEvents }) => {
                         {step === 2 && (
                             <>
                                 <Text fontSize='12'>
-                                    Fecha y hora del evento
+                                    {t('profileUser.events.2.dateTime')}
                                 </Text>
                                 <Flex w='full'>
                                     <Box w='50%'>
@@ -227,7 +230,7 @@ export const ButtonCreateEvent = ({ refreshEvents }) => {
                                             selected={date}
                                             onChange={(date) => setDate(date)}
                                             dateFormat="dd/MM/yyyy"
-                                            customInput={<Input placeholder="Fecha del evento" mb={4} />}
+                                            customInput={<Input placeholder={t('profileUser.events.2.dateTime')} mb={4} />}
                                         />
                                     </Box>
                                     <Box w='50%'>
@@ -244,14 +247,14 @@ export const ButtonCreateEvent = ({ refreshEvents }) => {
                                     </Box>
                                 </Flex>
                                 <Text fontSize='12' mb={2}>
-                                    Tipo de evento
+                                    {t('profileUser.events.2.type')}
                                 </Text>
                                 <Checkbox
                                     isChecked={types.includes('remote')}
                                     onChange={() => handleTypeChange('remote')}
                                     mb={2}
                                 >
-                                    Remoto
+                                    {t('remote')}
                                 </Checkbox>
                                 <Checkbox
                                     isChecked={types.includes('presential')}
@@ -259,14 +262,14 @@ export const ButtonCreateEvent = ({ refreshEvents }) => {
                                     mb={2}
                                     mx={2}
                                 >
-                                    Presencial
+                                    {t('presential')}
                                 </Checkbox>
 
                                 {types.includes('presential') && (
                                     <>
                                         {city && (
                                             <Text fontSize="sm" color="gray.500" mb={2}>
-                                                Ciudad: {city}
+                                                {t('profileUser.events.2.city')}: {city}
                                             </Text>
                                         )}
                                         <MapSearcher
@@ -283,16 +286,17 @@ export const ButtonCreateEvent = ({ refreshEvents }) => {
                         {step === 1 ? (
                             <>
                                 <Button variant='ghost' mr={3} onClick={closeModal}>
-                                    Cancelar
+                                    {t('cancel')}
                                 </Button>
-                                <Button colorScheme='gray' onClick={() => setStep(2)} disabled={!formCompleted()}>Siguiente</Button>
+                                <Button colorScheme='gray' onClick={() => setStep(2)} disabled={!formCompleted()}>{t('next')}</Button>
                             </>
                         ) : (
                             <>
                                 <Button variant='ghost' mr={3} onClick={() => setStep(1)} >
-                                    Volver
+                                    {t('back')}
                                 </Button>
-                                <Button colorScheme='gray' onClick={create} disabled={!formCompleted()}>Confirmar</Button>
+                                <Button colorScheme='gray' onClick={create} disabled={!formCompleted()}>
+                                    {t('confirm')}</Button>
                             </>
                         )}
                     </ModalFooter>
