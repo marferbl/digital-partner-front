a7b9d389e24a0ba325d2e5f56ddd8c2b1e823bba
Miguel Payá Vañó <biglodevelop@gmail.com>
2025-10-07 16:59:13 +0200

    [ADD] translations for empty-statejs

--- src/pages/admin/profile/empty-state.js
+++ src/pages/admin/profile/empty-state.js
@@ -1,18 +1,23 @@
 import { Box, Text, Center } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ButtonCreateCorporate } from "../../../components/corporate/button-create-corporate";
 import { FiDatabase } from "react-icons/fi";
 import { COLORS } from "../../../colors/colors";
+import { useTranslation } from "react-i18next";
 
 export const EmptyCorporateState = ({ refreshCorporate }) => {
+    const { t } = useTranslation("global");
+
     return (
         <Center flexDir={'column'} minH={'300'}>
             <FiDatabase size={70} color={'white'} />
-            <Text my={6} fontSize={24} color='white'>
-                Todavia no tienes una corporate
+            <Text mt={8} mb={4} fontSize={24} color='white'>
+                {t('profileUser.emptyStateTitle')}
+            </Text>
+            <Text mb={8} fontSize={16} color='white'>
+                {t('profileUser.emptyStateDescription')}
             </Text>
             <ButtonCreateCorporate refreshCorporate={refreshCorporate} />
         </Center>
-
-    )
+    );
 };
